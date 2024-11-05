0a214e11a80bb739a16b9c7e6423b008fc4efbfa
nmskfkai <leejimin00@naver.com>
2024-11-05 09:32:07 +0900

    api url, key 값 넘어오는지 post 확인

---
+++ src/main/java/com/example/apiWeather/ApiWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWeatherApplication.class, args);
+	}
+
+}
